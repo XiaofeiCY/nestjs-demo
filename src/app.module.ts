e07a315ab61b0b2fa5b0cf2204b8ae028c26cbce
春洋 <1141079850@qq.com>
2023-11-06 14:21:45 +0800

    chore: 通过nest g resource命令生成控制器等页面

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DemoController } from './demo/demo.controller';
+import { DemoModule } from './demo/demo.module';
+import { UserModule } from './user/user.module';
 
 /*
  * 类装饰器
  * 根模块用于处理其他类的引用与共享
  * */
 @Module({
-  imports: [],
+  imports: [DemoModule, UserModule],
   controllers: [AppController, DemoController],
   providers: [AppService],
 })
