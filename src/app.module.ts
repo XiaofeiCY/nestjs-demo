6af20098f1b89209dca163f37a2f398be0b10600
春洋 <1141079850@qq.com>
2023-11-07 20:11:33 +0800

    chore: 创建login功能模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,14 @@ import { DemoModule } from './demo/demo.module';
 import { UserModule } from './user/user.module';
 import { ListModule } from './list/list.module';
 import { UploadModule } from './upload/upload.module';
+import { LoginModule } from './login/login.module';
 
 /*
  * 类装饰器
  * 根模块用于处理其他类的引用与共享
  * */
 @Module({
-  imports: [DemoModule, UserModule, ListModule, UploadModule],
+  imports: [DemoModule, UserModule, ListModule, UploadModule, LoginModule],
   controllers: [AppController, DemoController],
   providers: [AppService],
 })
