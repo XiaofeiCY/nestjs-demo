8f4f92feb107b2ca0aca55d8c75055e2029d1b8e
春洋 <1141079850@qq.com>
2023-11-07 10:53:45 +0800

    chore: 注册并使用跨域依赖

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
 import * as session from 'express-session';
 import { NextFunction, Request, Response } from 'express';
+import * as cors from 'cors';
 // 项目的入口文件
 // 项目主函数
 /**
@@ -14,7 +15,9 @@ const blackList = ['/list'];
 function MiddleWareAll(req: Request, res: Response, next: NextFunction) {
   console.log('!!!这里是全局中间件!!!', req.originalUrl);
   if (blackList.includes(req.originalUrl)) {
-    res.send('被全局中间件拦截！！！');
+    res.send({
+      message: '被全局中间件拦截！！！',
+    });
   } else {
     next();
   }
@@ -23,6 +26,13 @@ function MiddleWareAll(req: Request, res: Response, next: NextFunction) {
 async function bootstrap() {
   //AppModule是主入口文件
   const app = await NestFactory.create(AppModule);
+  /**
+   * 注册跨域函数
+   * 测试方式：
+   * 打开一个浏览器控制台，输入：fetch('http://localhost:3000/list').then(res=>res.json()).then(res=>{console.log(res)})
+   * 就可以看到执行结果
+   */
+  app.use(cors());
   // 打开版本控制
   app.enableVersioning({
     type: VersioningType.URI,
