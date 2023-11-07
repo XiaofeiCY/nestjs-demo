40c38f923dce4121d8b5357c2d43e4fca20f347c
春洋 <1141079850@qq.com>
2023-11-07 20:10:24 +0800

    chore: 通过管道转换接口参数类型

--- src/list/list.controller.ts
+++ src/list/list.controller.ts
@@ -4,12 +4,17 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
+  ParseUUIDPipe,
   Patch,
   Post,
 } from '@nestjs/common';
 import { ListService } from './list.service';
 import { CreateListDto } from './dto/create-list.dto';
 import { UpdateListDto } from './dto/update-list.dto';
+import * as uuid from 'uuid';
+
+console.log('uuid', uuid.v4());
 
 @Controller('list')
 export class ListController {
@@ -25,9 +30,24 @@ export class ListController {
     return this.listService.findAll();
   }
 
+  /**
+   * 通过ParseIntPipe将id类型转为int类型
+   * @param id
+   */
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.listService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    console.log('===========', typeof id);
+    return this.listService.findOne(id);
+  }
+
+  /**
+   * 通过ParseUUIDPipe判断传入参数类型为uuid，校验不通过的时候接口报错
+   * @param id
+   */
+  @Get('/uuid/:id')
+  getUuid(@Param('id', ParseUUIDPipe) id: number) {
+    console.log('===========', typeof id);
+    return this.listService.findOne(id);
   }
 
   @Patch(':id')
