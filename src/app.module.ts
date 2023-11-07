f34beaa08fa3779802350c510a03650e6d01e68a
春洋 <1141079850@qq.com>
2023-11-07 11:02:14 +0800

    chore: 生成上传图片功能模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,14 @@ import { DemoController } from './demo/demo.controller';
 import { DemoModule } from './demo/demo.module';
 import { UserModule } from './user/user.module';
 import { ListModule } from './list/list.module';
+import { UploadModule } from './upload/upload.module';
 
 /*
  * 类装饰器
  * 根模块用于处理其他类的引用与共享
  * */
 @Module({
-  imports: [DemoModule, UserModule, ListModule],
+  imports: [DemoModule, UserModule, ListModule, UploadModule],
   controllers: [AppController, DemoController],
   providers: [AppService],
 })
