50711b4c4ef2b21dc80c8b5ff07626caed3e0a77
春洋 <1141079850@qq.com>
2023-11-07 09:46:46 +0800

    chore: 通过 nest g res list 创建list功能模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,14 @@ import { AppService } from './app.service';
 import { DemoController } from './demo/demo.controller';
 import { DemoModule } from './demo/demo.module';
 import { UserModule } from './user/user.module';
+import { ListModule } from './list/list.module';
 
 /*
  * 类装饰器
  * 根模块用于处理其他类的引用与共享
  * */
 @Module({
-  imports: [DemoModule, UserModule],
+  imports: [DemoModule, UserModule, ListModule],
   controllers: [AppController, DemoController],
   providers: [AppService],
 })
