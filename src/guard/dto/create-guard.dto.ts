d168bffa5fe347bd5da7c602acfdf6f7edbaf117
春洋 <1141079850@qq.com>
2023-11-08 10:27:53 +0800

    chore: swagger使用实例

--- src/guard/dto/create-guard.dto.ts
+++ src/guard/dto/create-guard.dto.ts
@@ -1,1 +1,8 @@
-export class CreateGuardDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateGuardDto {
+  @ApiProperty({ example: '蠢羊', required: true, type: 'string' }) // swagger接口参数描述，并提供默认值
+  name: string;
+  @ApiProperty({ example: 18 })
+  age: number;
+}
--- src/guard/guard.controller.ts
+++ src/guard/guard.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Param,
   Patch,
   Post,
@@ -13,9 +14,19 @@ import { CreateGuardDto } from './dto/create-guard.dto';
 import { UpdateGuardDto } from './dto/update-guard.dto';
 import { RoleGuard } from './role/role.guard';
 import { Role } from './role/role.decorator';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @Controller('guard')
 @UseGuards(RoleGuard)
+@ApiTags('守卫功能接口') //swagger接口分组
+@ApiBearerAuth() //swagger携带token，使用前需要在main.ts内全局注册addBearerAuth
 export class GuardController {
   constructor(private readonly guardService: GuardService) {}
 
@@ -27,11 +38,30 @@ export class GuardController {
   @Get()
   // @SetMetadata('roleList', ['admin', 'admin1'])
   @Role('admin', 'admin1')
+  @ApiOperation({ summary: 'Get接口', description: '获取全部guard数据' }) // swagger接口描述
+  @ApiQuery({
+    name: 'page',
+    description: '分页信息',
+  }) //swagger参数描述
+  @ApiResponse({
+    status: HttpStatus.FORBIDDEN,
+    description: '当借口报错403时的相关解释',
+  }) // swagger接口返回数据的描述
+  @ApiResponse({
+    status: HttpStatus.PERMANENT_REDIRECT,
+    description: '当借口报错308时的相关解释',
+  }) // swagger接口返回数据的描述
   findAll() {
     return this.guardService.findAll();
   }
 
   @Get(':id')
+  @ApiParam({
+    name: 'id',
+    description: '这个接口要传id',
+    required: true,
+    type: 'number',
+  }) // swagger参数描述
   findOne(@Param('id') id: string) {
     return this.guardService.findOne(+id);
   }
