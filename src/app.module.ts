146673834b962ed47bd094a2e8df62b3caaf0db5
春洋 <1141079850@qq.com>
2023-11-08 09:13:13 +0800

    chore: 添加守卫功能模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,14 @@ import { UserModule } from './user/user.module';
 import { ListModule } from './list/list.module';
 import { UploadModule } from './upload/upload.module';
 import { LoginModule } from './login/login.module';
+import { GuardModule } from './guard/guard.module';
 
 /*
  * 类装饰器
  * 根模块用于处理其他类的引用与共享
  * */
 @Module({
-  imports: [DemoModule, UserModule, ListModule, UploadModule, LoginModule],
+  imports: [DemoModule, UserModule, ListModule, UploadModule, LoginModule, GuardModule],
   controllers: [AppController, DemoController],
   providers: [AppService],
 })
