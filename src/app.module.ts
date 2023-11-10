67d21c0c6e8831c0e0ef8dfc5e96ab4072e5f963
春洋 <1141079850@qq.com>
2023-11-10 15:41:54 +0800

    chore: 连接数据库初始化

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,13 +8,36 @@ import { ListModule } from './list/list.module';
 import { UploadModule } from './upload/upload.module';
 import { LoginModule } from './login/login.module';
 import { GuardModule } from './guard/guard.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DbtestModule } from './dbtest/dbtest.module';
 
 /*
  * 类装饰器
  * 根模块用于处理其他类的引用与共享
  * */
 @Module({
-  imports: [DemoModule, UserModule, ListModule, UploadModule, LoginModule, GuardModule],
+  imports: [
+    DemoModule,
+    UserModule,
+    ListModule,
+    UploadModule,
+    LoginModule,
+    GuardModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql', // 数据库类型
+      username: 'root', // 账号
+      password: '123456', // 密码
+      host: 'localhost', // host
+      port: 3306, // 端口号
+      database: 'nestjsDB', // 库名
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // 实体文件
+      synchronize: true, // synchronize是否自动将实体类同步到数据库，建议生产环境不要用，开发环境无所谓了
+      retryDelay: 500, // 重试连接数据库间隔
+      retryAttempts: 10, // 重试连接数据库次数
+      autoLoadEntities: true, // 如果为true将自动加载实体forFeature()方法注册的每个实体都将自动添加到配置对象的实体数组中。
+    }),
+    DbtestModule,
+  ],
   controllers: [AppController, DemoController],
   providers: [AppService],
 })
