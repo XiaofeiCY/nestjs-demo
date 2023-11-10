97d413ee7f19c6a03a479ee481eff94b185bce54
春洋 <1141079850@qq.com>
2023-11-10 15:51:14 +0800

    chore: 连接数据库并创建表

--- src/dbtest/dbtest.module.ts
+++ src/dbtest/dbtest.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DbtestService } from './dbtest.service';
 import { DbtestController } from './dbtest.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Dbtest } from './entities/dbtest.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Dbtest])], // 关联数据库
   controllers: [DbtestController],
   providers: [DbtestService],
 })
--- src/dbtest/entities/dbtest.entity.ts
+++ src/dbtest/entities/dbtest.entity.ts
@@ -1,1 +1,17 @@
-export class Dbtest {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+/**
+ * 定义表
+ */
+@Entity()
+export class Dbtest {
+  // 主键自增
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  name: string;
+  @Column()
+  password: string;
+  @Column()
+  age: number;
+}
