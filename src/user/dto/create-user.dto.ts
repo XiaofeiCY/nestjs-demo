4fc5471f61d28f79ab6e47af8afbd8258804fffe
春洋 <1141079850@qq.com>
2023-11-12 10:36:52 +0800

    chore: 添加传参字段

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({
+    example: '蠢羊',
+    required: true,
+    type: 'string',
+  })
+  name: string;
+
+  @ApiProperty({
+    example: 18,
+    required: true,
+  })
+  age: number;
+
+  @ApiProperty({
+    example: '这是一段默认描述',
+  })
+  desc: string;
+}
