4dc6dbb8623984375b6b6dd42bf1805abb39bca1
春洋 <1141079850@qq.com>
2023-11-12 10:55:23 +0800

    chore: 添加传参字段

--- src/dbtest/dto/create-dbtest.dto.ts
+++ src/dbtest/dto/create-dbtest.dto.ts
@@ -1,1 +1,21 @@
-export class CreateDbtestDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateDbtestDto {
+  @ApiProperty({
+    example: '蠢羊',
+    required: true,
+    type: 'string',
+  })
+  name: string;
+
+  @ApiProperty({
+    example: 18,
+    required: true,
+  })
+  age: number;
+
+  @ApiProperty({
+    example: '这是一段默认描述',
+  })
+  desc: string;
+}
