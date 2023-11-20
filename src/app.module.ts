1be34f9443e1cd6e4cc5bfd4647c79674225e606
春洋 <1141079850@qq.com>
2023-11-20 11:52:11 +0800

    chore: 调整连接数据库属性

--- src/app.module.ts
+++ src/app.module.ts
@@ -29,13 +29,17 @@ import { ManageModule } from './manage/manage.module';
       username: 'root', // 账号
       password: '123456', // 密码
       host: 'localhost', // host
+      charset: 'utf8mb4', // or the appropriate character set
+      // collation: 'utf8mb4_unicode_ci', // or the appropriate collation
       port: 3306, // 端口号
       database: 'nestjsDB', // 库名
       entities: [__dirname + '/**/*.entity{.ts,.js}'], // 实体文件
-      synchronize: true, // synchronize是否自动将实体类同步到数据库，建议生产环境不要用，开发环境无所谓了，因为很耗内存，保存一次执行一次同步
+      // 注意：实体同步后会导致数据丢失，实操后发现中文数据经常会丢失，所以建议同步完关掉属性
+      // synchronize: true, // synchronize是否自动将实体类同步到数据库，建议生产环境不要用，开发环境无所谓了，因为很耗内存，保存一次执行一次同步
       retryDelay: 500, // 重试连接数据库间隔
       retryAttempts: 10, // 重试连接数据库次数
-      autoLoadEntities: true, // 如果为true将自动加载实体forFeature()方法注册的每个实体都将自动添加到配置对象的实体数组中。
+      // 注意：实体同步后会导致数据丢失，实操后发现中文数据经常会丢失，所以建议同步完关掉属性
+      // autoLoadEntities: true, // 如果为true将自动加载实体forFeature()方法注册的每个实体都将自动添加到配置对象的实体数组中。
     }),
     DbtestModule,
     ManageModule,
